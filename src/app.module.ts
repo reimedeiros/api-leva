ca4f2446f62f3b9716a5c36c8c6b81eee31bc9d9
Rei <rm091291@gmail.com>
2022-08-31 22:48:35 -0300

    Versionamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 @Module({
@@ -13,15 +12,5 @@ import { ConfigModule } from '@nestjs/config';
       `${process.env.MONGO_HOST}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB_NAME}/?retryWrites=true&w=majority`,
     ),
   ],
-=======
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
-import 'reflect-metadata';
-
-@Module({
-  imports: [UsersModule, DatabaseModule],
->>>>>>> f42f3be35c8d2d7b617eaa4740242949ef649cf5
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
